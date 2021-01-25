271aa2a20fd4859d96d3a409d31e33181996da6c
dy <>
2021-01-25 23:38:44 +0900

    menuitem bulkupdate 삭제 추가

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
@@ -1,10 +1,12 @@
 package kr.co.fastcampus.eatgo.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 @Getter
@@ -20,4 +22,8 @@ public class MenuItem {
     private Long restaurantId;
 
     private String name;
+
+    @Transient // DB에 안넣을 떄 사용하는 놈
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private boolean destroy;
 }
