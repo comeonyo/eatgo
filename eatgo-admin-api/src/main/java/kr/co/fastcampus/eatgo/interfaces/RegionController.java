6c0535844c49d02bbaf49de7f581c1278aa390cd
dy <>
2021-02-02 22:32:16 +0900

    region 추가

---
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RegionController.java
@@ -0,0 +1,42 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import kr.co.fastcampus.eatgo.application.RegionService;
+import kr.co.fastcampus.eatgo.domain.Region;
+import kr.co.fastcampus.eatgo.domain.Restaurant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+public class RegionController {
+
+    private RegionService regionService;
+
+    @Autowired
+    public RegionController(RegionService regionService) {
+        this.regionService = regionService;
+    }
+
+    @GetMapping("/regions")
+    public List<Region> list() {
+        return regionService.getRegions();
+    }
+
+    @PostMapping("/regions")
+    public ResponseEntity<?> create(@Valid @RequestBody Region resource)
+            throws URISyntaxException {
+
+        Region region = regionService.addRegion(resource.getName());
+
+        URI location = new URI("/regions/"+region.getId());
+        return ResponseEntity.created(location).body("{}");
+    }
+}
---
+++ eatgo-common/src/test/java/kr/co/fastcampus/eatgo/domain/RegionTest.java
@@ -0,0 +1,15 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegionTest {
+
+    @Test
+    public void creation() {
+        Region region = Region.builder().name("서울").build();
+
+        assertEquals(region.getName(), "서울");
+    }
+}
